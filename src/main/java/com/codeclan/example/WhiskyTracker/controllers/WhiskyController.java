25eba0d8faf8dd4d98f190e3a828900ccdbd822c
Azhar Sharif <azhar981@gmail.com>
2020-02-13 09:34:51 +0000

    Got stick on MVP but will redo and push on Thursday 13th Feb

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,27 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/whiskies")
+public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "whiskies/{year}")
+    public ResponseEntity<List<Whisky>> getByYear(@PathVariable int year){
+        return new ResponseEntity<>(whiskyRepository.findByYear(year), HttpStatus.OK);
+    }
+
+}
